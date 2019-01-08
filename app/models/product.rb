fdc5b54ac4e874a50fb75a8cfa4bdb4b2c547b5f
o0plamen <pmitev89@gmail.com>
2019-01-08 15:14:06 +0100

    5.2 Forms added conditional statement in product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,14 @@ class Product < ApplicationRecord
   has_many :orders
 
   def self.search(search_term)
+   if Rails.env == "development"
     Product.where("name LIKE ?", "%#{search_term}%")
+  elsif Rails.env == "production"
+    Product.where("name LIKE ?", "%#{search_term}%")
+  else
   end
-  
+
+  # Product.where("name ilike ?", "%#{search_term}%")
+  end
+
 end
