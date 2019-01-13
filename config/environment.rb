97fc097e9f423a08b4026301029e31bf131f10fe
o0plamen <pmitev89@gmail.com>
2019-01-13 18:53:51 +0100

    debugging

--- config/environment.rb
+++ config/environment.rb
@@ -3,15 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-
-config.action_mailer.delivery_method = :smtp
-# SMTP settings for gmail
-config.action_mailer.smtp_settings = {
- :address              => "smtp.gmail.com",
- :port                 => 587,
- :user_name            => ENV['gmail_username'],
- :password             => ENV['gmail_password'],
- :authentication       => "plain",
- :enable_starttls_auto => true
-}
