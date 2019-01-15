679fa4890c6c410bfee006ee55c653c6a2f6e1c2
o0plamen <pmitev89@gmail.com>
2019-01-15 13:38:06 +0100

    5.3 debugging

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ default from: "from@example.com"
   def contact_form(email, name, message)
   @message = message
       mail(from: email,
-      to: 'your-email@example.com',
+      to: 'books_berlin@abv.bg',
       subject: "A new contact form message from #{name}")
   end
 
