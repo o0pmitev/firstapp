cf5181955c7e2e25fb7dab2175035d06761b0691
Plamen Mitev <pmitev89@gmail.com>
2019-01-24 22:22:24 +0100

    create comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many :orders
-
+  has_many :comments
   def self.search(search_term)
    if Rails.env == "development"
     Product.where("name LIKE ?", "%#{search_term}%")
@@ -10,6 +10,10 @@ class Product < ApplicationRecord
   end
 
   # Product.where("name ilike ?", "%#{search_term}%")
+  end
+
+  def highest_rating_comment
+    comments.rating_desc.first
   end
 
 end
