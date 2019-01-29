760036c03e122317c923c57a333d93656b28388a
Plamen Mitev <pmitev89@gmail.com>
2019-01-29 22:44:46 +0100

    5.10 debugging prod env

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,7 +5,7 @@ class Ability
     user ||= User.new #guest user not logged in
     can :manage, User, id: user.id
     if user.admin?
-      can[:manage, :destroy], :all
+      can[:manage, :destroy, :all]
     else
       can :read, :all
     end
