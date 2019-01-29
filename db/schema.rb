fcf57fbac386ed797882f119fe521ed6a1766ab7
Plamen Mitev <pmitev89@gmail.com>
2019-01-29 22:31:40 +0100

    5.10 Admin Role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_24_103952) do
+ActiveRecord::Schema.define(version: 2019_01_29_205304) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2019_01_24_103952) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
